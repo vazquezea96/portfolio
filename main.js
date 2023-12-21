7cb088574e773f8f5baa088c0b3d43680d669744
vazquezea96 <vazquezeduardo1278@gmail.com>
2023-12-21 14:15:20 -0800

    updated main.js, game functions well

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ function initBoard() {
       box.className = "letter-box";
       row.appendChild(box);
     }
+
     board.appendChild(row);
   }
 }
@@ -33,13 +34,13 @@ document.addEventListener("keyup", (e) => {
     return;
   }
 
-  let presseKey = String(e.key);
+  let pressedKey = String(e.key);
   if (pressedKey === "Backspace" && nextLetter !== 0) {
     deleteLetter();
     return;
   }
 
-  if (pressedKey == "Enter") {
+  if (pressedKey === "Enter") {
     checkGuess();
     return;
   }
@@ -61,10 +62,7 @@ function insertLetter(pressedKey) {
   }
   pressedKey = pressedKey.toLowerCase();
 
-  let row =
-    document.getElementsByClassName("letter-row")[
-      NUMBER_OF_GUESSES - guessesRemaining
-    ];
+  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
   let box = row.children[nextLetter];
   box.textContent = pressedKey;
   box.classList.add("filled-box");
@@ -76,10 +74,7 @@ function insertLetter(pressedKey) {
 --------------------------------------> */
 
 function deleteLetter() {
-  let row =
-    document.getElementByClassName("letter-row")[
-      NUMBER_OF_GUESSES - guessesRemaining
-    ];
+  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
   let box = row.children[nextLetter - 1];
   box.textContent = "";
   box.classList.remove("filled-box");
@@ -91,10 +86,7 @@ function deleteLetter() {
 -----------------------------------------------> */
 
 function checkGuess() {
-  let row =
-    document.getElementsByClassName("letter-row")[
-      NUMBER_OF_GUESSES - guessesRemaining
-    ];
+  let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
   let guessString = "";
   let rightGuess = Array.from(rightGuessString);
 
@@ -103,7 +95,7 @@ function checkGuess() {
   }
 
   if (guessString.length != 5) {
-    alert("Not enought letters!");
+    alert("Not enough letters!");
     return;
   }
 
@@ -111,7 +103,78 @@ function checkGuess() {
     alert("Word not in list!");
     return;
   }
+
+  for (let i = 0; i < 5; i++) {
+    let letterColor = "";
+    let box = row.children[i];
+    let letter = currentGuess[i];
+
+    let letterPosition = rightGuess.indexOf(currentGuess[i]);
+    // is letter in the correct guess
+    if (letterPosition === -1) {
+      letterColor = "grey";
+    } else {
+      // now, letter is definitely in word
+      // if letter index and right guess index are the same
+      // letter is in the right position
+      if (currentGuess[i] === rightGuess[i]) {
+        // shade green
+        letterColor = "green";
+      } else {
+        // shade box yellow
+        letterColor = "yellow";
+      }
+
+      rightGuess[letterPosition] = "#";
+    }
+
+    let delay = 250 * i;
+    setTimeout(() => {
+      //shade box
+      box.style.backgroundColor = letterColor;
+      shadeKeyBoard(letter, letterColor);
+    }, delay);
+  }
+
+  if (guessString === rightGuessString) {
+    alert("You guessed right! Game over!");
+    guessesRemaining = 0;
+    return;
+  } else {
+    guessesRemaining -= 1;
+    currentGuess = [];
+    nextLetter = 0;
+
+    if (guessesRemaining === 0) {
+      alert("You've run out of guesses! Game over!");
+      alert(`The right word was: "${rightGuessString}"`);
+    }
+  }
 }
+
+/* Function to shade keyboard
+------------------------------> */
+
+function shadeKeyBoard(letter, color) {
+  for (const elem of document.getElementsByClassName("keyboard-button")) {
+    if (elem.textContent === letter) {
+      let oldColor = elem.style.backgroundColor;
+      if (oldColor === "green") {
+        return;
+      }
+
+      if (oldColor === "yellow" && color !== "green") {
+        return;
+      }
+
+      elem.style.backgroundColor = color;
+      break;
+    }
+  }
+}
+
+/* Generating output
+-----------------------------------------------> */
 
 document.getElementById("keyboard-cont").addEventListener("click", (e) => {
   const target = e.target;
