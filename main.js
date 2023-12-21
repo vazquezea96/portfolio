acf207ad66441455c1ffef40277a58bb56c05363
vazquezea96 <vazquezeduardo1278@gmail.com>
2023-12-21 07:01:55 -0800

    updated main.js file

--- main.js
+++ main.js
@@ -112,3 +112,18 @@ function checkGuess() {
     return;
   }
 }
+
+document.getElementById("keyboard-cont").addEventListener("click", (e) => {
+  const target = e.target;
+
+  if (!target.classList.contains("keyboard-button")) {
+    return;
+  }
+  let key = target.textContent;
+
+  if (key === "Del") {
+    key = "Backspace";
+  }
+
+  document.dispatchEvent(new KeyboardEvent("keyup", { key: key }));
+});
